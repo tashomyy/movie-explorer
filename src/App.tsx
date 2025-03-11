9f7c18e402056ef8df7de5f9b367b25204c9d358
Natasa <tashomyy@gmail.com>
2025-03-11 19:40:51 +0100

    created dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p:4 lg:p-12">
       <h1 className="text-xl font-bold">This is the best Movie explorer app</h1>
-      <p className="my-8">Welcome to the Initial commit</p>
+      <p className="my-8">Setting up the dev branch</p>
       <img
         className="w-144"
         src="/movie-player.svg"
