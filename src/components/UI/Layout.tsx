5e89c887eff70b200b9a31a19e67a3eacc4c65ba
Natasa <tashomyy@gmail.com>
2025-03-13 19:39:15 +0100

    implemented padding on smaller screens

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 const Layout = () => {
-  const classNameWidth = "w-full max-w-7xl mx-auto";
+  const classNameWidth = "w-full max-w-7xl mx-auto px-4 md:px-8";
   return (
     <div className="min-h-screen flex flex-col gap-8 w-full">
       <Header classNameWidth={classNameWidth} />
