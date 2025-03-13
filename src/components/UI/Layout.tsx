30f589320658ae67baf776012b5e598ef9f44bbf
Natasa <tashomyy@gmail.com>
2025-03-13 19:43:17 +0100

    implemented apply class in index for container

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -2,11 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 const Layout = () => {
-  const classNameWidth = "w-full max-w-7xl mx-auto px-4 md:px-8";
   return (
     <div className="min-h-screen flex flex-col gap-8 w-full">
-      <Header classNameWidth={classNameWidth} />
-      <main className={`h-full ${classNameWidth}`}>
+      <Header classNameWidth="container" />
+      <main className="h-full container">
         <Outlet />
       </main>
       <footer className="mt-auto">My App Footer</footer>
