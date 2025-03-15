06194336bc1c19c6248eb3c562d054c30e23813c
Natasa <tashomyy@gmail.com>
2025-03-15 23:13:03 +0100

    implemented App providers in main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import AppRoutes from "./Routes";
-import AppProviders from "./store/AppProvider";
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <ToastContainer />
-      <AppProviders>
-        <AppRoutes />
-      </AppProviders>
+      <AppRoutes />
     </div>
   );
 }
