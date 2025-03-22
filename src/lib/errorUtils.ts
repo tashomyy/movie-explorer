f267a0da01f518079d5aef3f8933b34a04a0ea8e
Natasa <tashomyy@gmail.com>
2025-03-23 00:48:07 +0100

    implemented centralizing error handling in interceptors and added retry if the error code is higher than or equal to 500

--- src/lib/errorUtils.ts
+++ src/lib/errorUtils.ts
@@ -1,12 +1,8 @@
-import { toast } from "react-toastify";
-
 export const handleError = (error: any, customMessage: string) => {
   const errorMessage =
     error?.response?.data?.message ||
     customMessage ||
     "An unexpected error occurred.";
-
-  toast.error(errorMessage);
 
   throw new Error(errorMessage);
 };
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { toast } from "react-toastify";
 
 export const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
 
@@ -19,6 +20,28 @@ apiClient.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const config = error.config;
+
+    const status = error.response?.status;
+    const message = error.message;
+    const url = config?.url || "unknown endpoint";
+
+    console.error(`[API Error] ${status} @ ${url} → ${message}`);
+    toast.error(message);
+
+    if (!config._retry && status >= 500) {
+      config._retry = true;
+
+      return apiClient(config);
+    }
+
     return Promise.reject(error);
   }
 );
